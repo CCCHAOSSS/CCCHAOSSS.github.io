145989718bd4f120578c8697566ef541aeccae1d
CHAOS <lmliang11@foxmail.com>
2024-04-27 21:01:39 +0800

    Site updated: 2024-04-27 21:01:35

---
+++ dist/music.js
@@ -0,0 +1,12 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    auoplay: false,
+    audio: [
+        {
+            name: 'Call you',
+            artist: '山田タマル',
+            url: '//music.163.com/song/media/outer/url?id=489970553.mp3',
+        }
+    ]
+});
